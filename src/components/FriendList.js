d4d2153ab3f8f2092213f1e5b13575f769edd696
Su-daa <subin.kk019@gmail.com>
2023-08-18 00:16:00 +0900

    Feat: 친구목록 보여주기

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -14,14 +14,14 @@ function FriendList(props) {
     <div>
       <div className="image-container">
         <img
-          src={friendListImg}
+          src={props.friend.getS3Res.imgUrl}
           alt="friendListImg"
           className="friendListImg"
         />
         <div>
           <h3 className="container">
-            <h2 className="container">{props.friendList.nickname}</h2>
-            {props.friendList.birth}&nbsp; ({props.friendList.gender})
+            <h2 className="container">{props.friend.nickname}</h2>
+            {props.friend.birth}&nbsp; ({props.friend.gender})
           </h3>
           <button className="callButton" onClick={handleCallButtonClick}>
             <div className="iconImg">
